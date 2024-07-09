fe506e5bc2edc2ed29e6179d8c11f245b0ba174e
Maksim <121749232+MaksimCh1988@users.noreply.github.com>
2024-07-09 22:29:42 +0300

    finish

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,13 @@
+import PromoText from './PromoText';
+import MealList from './MealsList';
+
+const Meals = () => {
+  return (
+    <>
+      <PromoText />
+      <MealList />
+    </>
+  );
+};
+
+export default Meals;
